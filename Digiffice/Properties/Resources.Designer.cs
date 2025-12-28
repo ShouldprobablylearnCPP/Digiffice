c3eededb6b6b52136020e2f8d7eedd808c400359
ShouldprobablylearnCPP <cole.ryan1511@gmail.com>
2025-12-28 23:59:23 +0000

    Final code commit for 28/12/2025 - Created Image for new notebook button

--- Digiffice/Properties/Resources.Designer.cs
+++ Digiffice/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Digiffice.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap NewNotebookBtn {
+            get {
+                object obj = ResourceManager.GetObject("NewNotebookBtn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Panel {
             get {
                 object obj = ResourceManager.GetObject("Panel", resourceCulture);
--- Digiffice/Resources/Classes/ProgramClasses/DigifficeAllnote/AllnoteTabClasses/DigifficeAllnoteFileTab.cs
+++ Digiffice/Resources/Classes/ProgramClasses/DigifficeAllnote/AllnoteTabClasses/DigifficeAllnoteFileTab.cs
@@ -15,6 +15,7 @@ namespace Digiffice.Resources.Classes.ProgramClasses.DigifficeAllnote.AllnoteTab
         public void InitialiseUI(Panel rbnPnl)
         {
             // NewFileBtn
+            NewAllnoteFileBtn.Name = "NewAllnoteFileButton";
             NewAllnoteFileBtn.Enabled = true;
 
             NewAllnoteFileBtn.Size = new Size(110,110);
@@ -22,12 +23,27 @@ namespace Digiffice.Resources.Classes.ProgramClasses.DigifficeAllnote.AllnoteTab
             NewAllnoteFileBtn.Text = "New Notebook";
             NewAllnoteFileBtn.TextAlign = ContentAlignment.BottomCenter;
             NewAllnoteFileBtn.Font = new Font("Roboto", 8, FontStyle.Regular);
-            // insert background image here
+            NewAllnoteFileBtn.Image = Properties.Resources.NewNotebookBtn;
+            NewAllnoteFileBtn.ImageAlign = ContentAlignment.MiddleCenter;
+            NewAllnoteFileBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            NewAllnoteFileBtn.BackColor = Color.Transparent;
 
             NewAllnoteFileBtn.FlatStyle = FlatStyle.Flat;
             NewAllnoteFileBtn.FlatAppearance.BorderSize = 0;
+            NewAllnoteFileBtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            NewAllnoteFileBtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+
+            NewAllnoteFileBtn.Cursor = Cursors.Hand;
+
+            NewAllnoteFileBtn.Click += new EventHandler(NewAllnoteFileButton_Click);
 
             rbnPnl.Controls.Add(NewAllnoteFileBtn);
+        }
+
+        public void NewAllnoteFileButton_Click(object sender, EventArgs e)
+        {
+            // insert functionality here
+            MessageBox.Show("New Notebook button clicked!", "", MessageBoxButtons.OK);
         }
     }
 }
