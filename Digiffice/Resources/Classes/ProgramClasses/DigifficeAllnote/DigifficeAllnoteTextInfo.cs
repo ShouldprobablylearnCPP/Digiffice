11d5280da4b89d2bea280b927adc359a79154b4a
ShouldprobablylearnCPP <cole.ryan1511@gmail.com>
2026-09-06 21:20:46 +0100

    Final Code commit for 6/9/2026.
    Partially implemented DigifficeAllnoteTextInfo.cs.

---
+++ Digiffice/Resources/Classes/ProgramClasses/DigifficeAllnote/DigifficeAllnoteTextInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digiffice.Resources.Classes.ProgramClasses.DigifficeAllnote
+{
+    public class DigifficeAllnoteTextInfo
+    {
+        // Class Variables
+        public int txtSize = 8;
+        public Color txtColor = Color.Black;
+        public Color txtHighlightColor = Color.Transparent;
+        public bool txtBullet = false;
+
+        public FontFamily fontFamily = new FontFamily("Roboto");
+        public FontStyle fontWeight = FontStyle.Regular;
+        public FontStyle fontItallic = FontStyle.Regular;
+        public FontStyle fontUnderline = FontStyle.Regular;
+        public FontStyle fontStrike = FontStyle.Regular;
+
+        // Presenter
+        public void updateSelectedText(RichTextBox rtb)
+        {
+
+        }
+
+        public void updateTextFormatMenu()
+        {
+
+        }
+    }
+}
